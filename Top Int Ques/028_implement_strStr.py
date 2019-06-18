9f6802c8635d5a4dfcd872b209fba22b1fd1b319
Rajarshi Basak <rajarshi.basak@colorado.edu>
2019-06-18 12:58:50 -0600

    Create 028_implement_strStr.py
    Implement strStr().
    
    Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
    
    Example 1:
    
    Input: haystack = "hello", needle = "ll"
    Output: 2
    Example 2:
    
    Input: haystack = "aaaaa", needle = "bba"
    Output: -1
    Clarification:
    
    What should we return when needle is an empty string? This is a great question to ask during an interview.
    
    For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().

---
+++ Top Int Ques/028_implement_strStr.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+        if not needle:
+            return 0
+        
+        if needle in haystack:
+            ind = haystack.index(needle)
+            return ind
+        else:
+            return -1
